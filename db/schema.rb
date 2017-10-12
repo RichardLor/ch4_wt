ada92a148150b82ed66d4bb1e207a9de388d83a1
Your Name <rlor1@uncc.edu>
2017-10-12 00:57:41 +0000

    third commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
+#check
 ActiveRecord::Schema.define(version: 20150809022253) do
 
   create_table "movies", force: :cascade do |t|
